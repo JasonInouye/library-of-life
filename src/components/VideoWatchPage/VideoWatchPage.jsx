fff5ddd5964d749312f7055623a80dd526f11e7d
Michael Chuinard <mpchuinard@finlandia.edu>
2022-05-18 13:40:30 -0500

    add share dropdown to video watch page component

--- src/components/VideoWatchPage/VideoWatchPage.jsx
+++ src/components/VideoWatchPage/VideoWatchPage.jsx
@@ -9,13 +9,45 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { VscTrash } from "react-icons/vsc";
+import { SiSlideshare } from "react-icons/si";
 
 /******* general MUI structure  ********/
 import Container from '@mui/material/Container';
 
 function VideoWatchPage() {
 
+    const user = useSelector((store) => store.user);
 
+
+    //The database will replace this useState
+    const [permission, setPermission] = useState('');
+
+    const handleChange = (event) => {
+
+        //Change to dispatch when hooked up to DB
+        setPermission(event.target.value);
+    };
+
+
+
+
+    const [menuPosition, setMenuPosition] = useState(null);
+
+    const openShareMenu = (event) => {
+        if (menuPosition) {
+            return;
+        }
+        event.preventDefault();
+        setMenuPosition({
+            top: event.pageY,
+            left: event.pageX
+        });
+    };
+
+    const handleItemClick = () => {
+        setMenuPosition(null);
+    };
 
 
     return (
@@ -24,10 +56,67 @@ function VideoWatchPage() {
                 <h2 className="border sage">If you could do something to change humanity, what would it be?</h2>
                 <h5 className="sage">--video goes here--</h5>
 
-                <MenuItem value={4}>Everyone</MenuItem>
-                <MenuItem value={3}>Friends</MenuItem>
-                <MenuItem value={2}>Family</MenuItem>
-                <MenuItem value={1}>Invite-Only</MenuItem>
+                < FormControl sx={{ m: 1, minWidth: 180 }} size="small">
+                <InputLabel id="permission-select-small">Who can see this?</InputLabel>
+                <Select
+                    labelId="permission-select-small"
+                    id="permission-select-small"
+                    value={permission}
+                    label="permission"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={4}>Everyone</MenuItem>
+                    <MenuItem value={3}>Friends</MenuItem>
+                    <MenuItem value={2}>Family</MenuItem>
+                    <MenuItem value={1}>Invite-Only</MenuItem>
+                </Select>
+            </FormControl>
+
+            {/* share button */}
+            <div id='share'>
+                <Button
+                    variant='contained'
+                    onClick={openShareMenu}>
+                    Share <span style={{ paddingLeft: '5px' }}><SiSlideshare /> </span>
+                </Button>
+                <Menu
+                    open={!!menuPosition}
+                    onClose={() => setMenuPosition(null)}
+                    anchorReference="anchorPosition"
+                    anchorPosition={menuPosition}
+                >
+                    <NestedMenuItem
+                        label="Friends"
+                        parentMenuOpen={!!menuPosition}
+                        onClick={handleItemClick}
+                    >
+                        <MenuItem onClick={handleItemClick}>All Friends</MenuItem>
+                        <br />
+                        <MenuItem onClick={handleItemClick}>Dave</MenuItem>
+                    </NestedMenuItem>
+                    <NestedMenuItem
+                        label="Family"
+                        parentMenuOpen={!!menuPosition}
+                        onClick={handleItemClick}
+                    >
+                        <MenuItem onClick={handleItemClick}>All Family</MenuItem>
+                        <br />
+                        <MenuItem onClick={handleItemClick}>Mom</MenuItem>
+                    </NestedMenuItem>
+                </Menu>
+                <Button
+                    variant='contained'
+                    color='error'>
+                    <span style={{ marginTop: '5px' }}>
+                        <VscTrash size={17} /></span>
+                </Button>
+
+                {/* :
+                null} */}
+
+            </div >
+
+                
                 <Button variant='outlined' sx={{ margin: '20px', color: 'white', backgroundColor: '#667b68' }}>Delete</Button>
                 <Button variant='outlined' sx={{ margin: '20px', color: 'white', backgroundColor: '#667b68' }}>Share</Button>
                 <Button variant='outlined' sx={{ margin: '20px', color: 'white', backgroundColor: '#667b68' }}>Back</Button>
