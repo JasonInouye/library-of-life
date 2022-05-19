ca98c7ff0fd2f2ef8f0d0b7a6011b6e27bbded36
Jason Inouye <jasonjinouye@gmail.com>
2022-05-18 23:45:58 -0500

    added uploader component and deleted image validator files

--- src/components/VideoUploadPage/VideoUploadPage.jsx
+++ src/components/VideoUploadPage/VideoUploadPage.jsx
@@ -3,14 +3,14 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import UploadForm from './UploadForm';
+import Uploader from './Uploader';
 
 function VideoUploadPage() {
     return (
         <main className="sageBackground">
             <div className="videoWatch">
                 <h2 className="border sage">If you could do something to change humanity, what would it be?</h2>
-                <UploadForm />
+                <Uploader />
                 <Button variant='outlined' sx={{ margin: '20px', color: 'white', backgroundColor: '#667b68' }}>Save</Button>
                 <Button variant='outlined' sx={{ margin: '20px', color: 'white', backgroundColor: '#667b68' }}>Back</Button>
             </div>
