41dc495b7bb0c5921d03843a2f1fae37a10adb4b
Lucas Houska <ljhouska75@gmail.com>
2022-05-19 13:44:50 -0500

    filtered user from list of users

--- src/redux/reducers/search.reducer.js
+++ src/redux/reducers/search.reducer.js
@@ -1,23 +1,12 @@
 const listOfUsers = (state = [], action) => {
     switch (action.type) {
-      case 'SET_LIST_OF_USERS':
-          let listOfUsersMinusYourself = [];
-          const currentUser = action.payload.currentUser.id
-
-          console.log('payload', action.payload);
-          console.log('search stuff', action.payload.listOfUsers);
-          console.log('user id in reducer', currentUser);
-
-          listOfUsersMinusYourself = action.payload.listOfUsers
-
-          listOfUsersMinusYourself.filter(searchedUser => searchedUser.id != currentUser);          
-
-          console.log('list of users', action.payload);
-          
-        return listOfUsersMinusYourself;
-      default:
-        return state;
+        case 'SET_LIST_OF_USERS':
+            const listOfUsersMinusYourself = action.payload.listOfUsers;
+            const currentUser = action.payload.currentUser.id;
+            return listOfUsersMinusYourself.filter(searchedUser => searchedUser.id != currentUser);;
+        default:
+            return state;
     }
-  }
+}
 
-  export default listOfUsers;
+export default listOfUsers;
