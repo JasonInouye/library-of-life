f27c93137708f9b9bb50b48a487408ce7f7d7977
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-05-19 10:01:25 -0500

    change query in USER VIDEO GET to also get prompts

--- server/routes/video.router.js
+++ server/routes/video.router.js
@@ -6,23 +6,25 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+    // GET route code here
 });
 
 /**
- * GET route for ALL USER videos
+ * GET route for ALL USER videos and their prompts
  */
 router.get('/userVideos/:id', (req, res) => {
     const query = `
-    SELECT * FROM "videos"
-    WHERE "user_id" = $1; 
+    SELECT "videos".url, "prompts".prompt
+FROM "videos" JOIN "prompts"
+ON "videos"."user_id" = "prompts"."user_id"
+WHERE "videos"."user_id" = $1; 
     `;
 
-    console.log('server GET userVideos', req.user.id)
+    console.log('server GET userVideos and prompts', req.user.id)
     pool.query(query, [req.user.id]).then((result) => {
         res.send(result.rows);
     }).catch(err => {
-        console.log('ERROR: Get one treat', err);
+        console.log('ERROR: Get video URLs and prompts,', err);
         res.sendStatus(500)
     });
 });
@@ -30,15 +32,15 @@ router.get('/userVideos/:id', (req, res) => {
 /**
  * GET route for SINGLE video
  */
- router.get('/', (req, res) => {
+router.get('/', (req, res) => {
     // GET route code here
-  });
+});
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
-  // POST route code here
+    // POST route code here
 });
 
 module.exports = router;
