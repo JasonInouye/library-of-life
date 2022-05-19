beae8a4a01dfda787819a8ce7ccb5c5544cb06e3
Anissa Crawford <anissacrawford@gmail.com>
2022-05-19 11:03:15 -0500

    add toggle connection functionality

--- server/routes/connections.router.js
+++ server/routes/connections.router.js
@@ -4,10 +4,20 @@ const router = express.Router();
 
 //get connections from DB 
 router.get('/', (req, res) => {
-    const queryText = `SELECT * FROM "connections" a, "users" b
-    WHERE a."user_B_id" = b."id";`;
+    const queryText = `
+    SELECT 
+    b.first_name,
+    b.last_name,
+    a.relationship
+    FROM "connections" a, "users" b
+    WHERE (a."user_A_id" = $1
+    and a."user_B_id" = b."id") OR (a."user_B_id" = $2
+    and a."user_A_id" = b."id") 
+    ;`;
 
-    pool.query(queryText)
+    const queryValues = [req.user.id, req.user.id];
+
+    pool.query(queryText, queryValues)
     .then((result) => {
         console.log('server GET connections', result.rows);
         res.send(result.rows);
--- src/components/Connections/Connections.jsx
+++ src/components/Connections/Connections.jsx
@@ -1,35 +1,77 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './Connections.css';
+import { ToggleButton } from '@mui/material';
+import { ToggleButtonGroup } from '@mui/material';
 
-function Connections () {
+function Connections() {
 
     const dispatch = useDispatch();
-
     const connections = useSelector((store) => store.connectionsReducer);
-    console.log(connections);
+
+    const [toggle, setToggle] = React.useState('left');
+    const [friends, setFriends] = useState(false);
+    const [family, setFamily] = useState(false);
+
+    const handleToggle = (event, newToggle) => {
+        setToggle(newToggle);
+    };
+
+    const handleFriends = () => {
+        setFriends(true);
+        setFamily(false);
+    };
+
+    const handleFamily = () => {
+        setFriends(false);
+        setFamily(true);
+    };
 
     useEffect(() => {
-        dispatch({type: 'GET_CONNECTIONS'})
-    }, [])
+        dispatch({ type: 'GET_CONNECTIONS' })
+    }, []);
+
 
     return (
         <>
+            {/* handle friends */}
+             
+            {friends && connections?.map((connect, i) => {
+                return (
+                    <div key={i}>
+                        {connect.relationship == "friend" &&
+                            <ul>
+                                <li>{connect.first_name + " " + connect.last_name}</li>
+                            </ul>}
+                    </div>
+                )
+            })}
 
-        {/* FRIEND: if user a id = user id  && relationship = friend, show user b */}
-        {/* FAMILY: if user a id = user id  && relationship = family, show user b */}
-        {/* ALL: if user a id = user id, show user b */}
+            {/* handle family */}
+            {family && connections?.map((connect, i) => {
+                return (
+                    <div key={i}>
+                        {connect.relationship == "family" &&
+                            <ul>
+                                <li>{connect.first_name + " " + connect.last_name}</li>
+                            </ul>}
+                    </div>
+                )
+            })}
 
+            <ToggleButtonGroup
+                value={toggle}
+                exclusive
+                onChange={(event) => { handleToggle(event.target.value) }}
+                aria-label="connections">
+                <ToggleButton onClick={() => { handleFriends() }} value="friends" aria-label="left aligned">
+                    <h1>Friends</h1>
+                </ToggleButton>
+                <ToggleButton onClick={() => { handleFamily() }} value="family" aria-label="centered">
+                    <h1>Family</h1>
+                </ToggleButton>
+            </ToggleButtonGroup>
 
-        {connections?.map((connect) => {
-            return (
-                // {connect.relationship = "family"  }
-                <ul>
-                    <li>{connect.first_name + " " + connect.last_name}</li>
-                </ul>
-            )
-        })}
-        
         </>
     )
 }
