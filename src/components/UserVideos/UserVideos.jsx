6a553add0644d703e1714cd9866bea89b75eba46
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-05-22 21:12:15 -0500

    recreate ReactPlayerComponent to fix file problem

--- src/components/UserVideos/UserVideos.jsx
+++ src/components/UserVideos/UserVideos.jsx
@@ -42,22 +42,3 @@ function UserVideos() {
 
 export default UserVideos;
 
-import React from 'react';
-
-/******* video player  ********/
-import ReactPlayer from 'react-player';
-
-
-function ReactPlayerComponent({videoURL}) {
-    
-    return (
-        <ReactPlayer
-            className='react-player'
-            width='100%'
-            height='100%'
-            url={videoURL}
-            controls={true} />
-    );
-}
-
-export default ReactPlayerComponent;
