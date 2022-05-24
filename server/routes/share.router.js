8095d90b41d2281ad3bce996a02c42da5f4cfc75
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-05-24 16:58:10 -0500

    add async in share router

--- server/routes/share.router.js
+++ server/routes/share.router.js
@@ -2,18 +2,45 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    const queryText = `
-        INSERT INTO "shared_videos" ("user_id", "video_id")
-        VALUES ($1, $2)
-  ;`
+router.post('/', async (req, res) => {
+    //loop over the data and make SQL statements
+    const userIdArray = req.body.user_id;
+    const videoId = req.body.video_id;
 
-    pool.query(queryText).then(result => {
-        res.send(result.rows);
-    }).catch(error => {
-        console.log(error);
+    // Set up continuous connection for all queries...
+    const connection = await pool.connect()
+
+    try {
+        await connection.query('BEGIN');
+        // TODO loop here --- await connection.query(sqlText, []);
+
+
+        // const sqlText = `INSERT INTO "shared_videos" ("user_id", "video_id")
+        // VALUES ($1, $2)`;
+
+        for (const userID of userIdArray) {
+            const sqlText = 
+                `INSERT INTO "shared_videos" ("user_id", "video_id")
+                VALUES (${userID}, ${videoId})`;
+            await connection.query(sqlText)
+        }
+
+
+        await connection.query('COMMIT');
+        res.sendStatus(200);
+        console.log(status);
+        
+    } catch (error) {
+        await connection.query('ROLLBACK');
+        console.log(`Transaction Error - Rolling back transfer`, error);
         res.sendStatus(500);
-    })
+    } finally {
+        // Always runs - both after successful try & after catch
+        // Put the client connection back in the pool
+        // This is super important! 
+        connection.release()
+    }
+
 });
 
 
