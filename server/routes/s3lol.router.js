ad5cc3c6bc798a81ec2a83367b69bcb443351479
Jason Inouye <jasonjinouye@gmail.com>
2022-05-24 09:18:59 -0500

    removed old code from s3 router

--- server/routes/s3lol.router.js
+++ server/routes/s3lol.router.js
@@ -3,13 +3,6 @@ const axios = require('axios');
 const router = express.Router();
 const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
-// router.get('/image', rejectUnauthenticated, async (req, res) => {
-//     // GET route code here
-//     let mediaType = '.jpeg';
-//     const url = await s3Url(mediaType);
-//     res.send({url})
-// });
-
 router.get('/', rejectUnauthenticated, async (req, res) => {
     console.log('API S3 URL request');
     const API_ENDPOINT ='https://hfoxt7tc91.execute-api.us-east-1.amazonaws.com/default/getPresignedVideoURL2';
