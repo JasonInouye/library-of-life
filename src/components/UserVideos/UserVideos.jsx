0dbc9a726eb681504d9a654a6fea2d2e83cb9a91
Lucas Houska <ljhouska75@gmail.com>
2022-05-26 12:08:03 -0500

    g

--- src/components/UserVideos/UserVideos.jsx
+++ src/components/UserVideos/UserVideos.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
@@ -17,7 +17,7 @@ function UserVideos({ relationship }) {
     const user = useSelector((store) => store.user);
     const videos = useSelector((store) => store.videoReducer);
     const [toggle, setToggle] = React.useState('left');
-    const [myVideos, setMyVideos] = useState(false);
+    const [myVideos, setMyVideos] = useState(true);
     const [mySharedVideos, setSharedVideos] = useState(false);
 
     // const searchedUserVideos = useSelector((store) => store.searchedUserVideos)
@@ -30,7 +30,7 @@ function UserVideos({ relationship }) {
         if (user.id == userInParams) {
             dispatch({ type: 'GET_USER_VIDEOS' });
         } else {
-            dispatch({ type: 'GET_SEARCHED_USER_VIDEOS', payload: userInParams})
+            dispatch({ type: 'GET_SEARCHED_USER_VIDEOS', payload: userInParams })
         }
     }, []);
 
@@ -52,39 +52,64 @@ function UserVideos({ relationship }) {
     return (
         <>
             <Container>
-              <div className="moveRight">  <ToggleButtonGroup
-                    value={toggle}
-                    className="videoToggle"
-                    size="small"
-                    exclusive
-                    onChange={(event) => { handleToggle(event.target.value) }}
-                    aria-label="connections">
-                    <ToggleButton onClick={() => { handleMyVideos() }} value="myVideos" aria-label="myVideos">
-                        <h3>My Videos</h3>
-                    </ToggleButton>
+                <div className="moveRight">
+                    <ToggleButtonGroup
+                        value={toggle}
+                        className="videoToggle"
+                        size="small"
+                        exclusive
+                        onChange={(event) => { handleToggle(event.target.value) }}
+                        aria-label="connections">
+                        <ToggleButton onClick={() => { handleMyVideos() }} value="myVideos" aria-label="myVideos">
+                            <h3>My Videos</h3>
+                        </ToggleButton>
 
-                    <ToggleButton onClick={() => { handleSharedVideos() }} value="sharedVideos" aria-label="sharedVideos">
-                        <h3>Shared Videos</h3>
-                    </ToggleButton>
-                </ToggleButtonGroup> </div>
+                        <ToggleButton onClick={() => { handleSharedVideos() }} value="sharedVideos" aria-label="sharedVideos">
+                            <h3>Shared Videos</h3>
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                </div>
 
-                <Grid container
-                    style={{ padding: '1em', textAlign: 'center' }}
-                    spacing={1}>
 
-                    {videos?.map((video, i) => {
-                        return ( //loops thru array of videos to create each video item
-                            < Grid
-                                item xs={12} md={4}
-                                key={video.id}>
+                {myVideos &&
+                    <Grid container
+                        style={{ padding: '1em', textAlign: 'center' }}
+                        spacing={1}>
 
-                                <UserVideoItem
-                                    key={i}
-                                    video={video}
-                                    relationship={relationship} />
-                            </Grid>)
-                    })}
-                </Grid>
+                        {videos?.map((video, i) => {
+                            return ( //loops thru array of videos to create each video item
+                                < Grid
+                                    item xs={12} md={4}
+                                    key={video.id}>
+
+                                    <UserVideoItem
+                                        key={i}
+                                        video={video}
+                                        relationship={relationship} />
+                                </Grid>)
+                        })}
+                    </Grid>
+                }
+
+                {sharedVideos &&
+                    <Grid container
+                        style={{ padding: '1em', textAlign: 'center' }}
+                        spacing={1}>
+
+                        {videos?.map((video, i) => {
+                            return ( //loops thru array of videos to create each video item
+                                < Grid
+                                    item xs={12} md={4}
+                                    key={video.id}>
+
+                                    <UserVideoItem
+                                        key={i}
+                                        video={video}
+                                        relationship={relationship} />
+                                </Grid>)
+                        })}
+                    </Grid>
+                }
             </Container>
         </>
     )
