6766b41a68e2f6665019ce11a01f797f37c17e20
Jason Inouye <jasonjinouye@gmail.com>
2022-05-28 10:04:21 -0500

    removed logout history push

--- src/components/_Widgets/LogOutButton.jsx
+++ src/components/_Widgets/LogOutButton.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Button } from '@mui/material';
+import {useHistory} from 'react-router-dom'
 
 function LogOutButton(props) {
+  const history = useHistory ();
   const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch({ type: 'LOGOUT' })
+  }
+
   return (
     <Button
       variant='outlined'
       size='small'
       sx={{ margin: '.5em' }}
-      onClick={() => dispatch({ type: 'LOGOUT' })}>
+      onClick={handleLogout}
+      >
       Log Out
     </Button>
   );
