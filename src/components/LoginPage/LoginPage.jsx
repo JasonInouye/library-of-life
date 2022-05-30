a68f37bfcc6637773c0022218a8a1a3fec357941
Lucas Houska <ljhouska75@gmail.com>
2022-05-30 12:29:47 -0500

    cleared url to "login" upon logout

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
+import {useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import './LoginPage.css';
 
 function LoginPage() {
+
+  const history = useHistory();
+
+  const goToLogin = () => {
+    history.push('/login');
+  }
+
+  useEffect(() => {
+    setTimeout(goToLogin, 500);
+}, [])
 
   return (
     <div>
